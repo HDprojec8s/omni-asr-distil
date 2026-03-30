9cb59b26c34cf2b8431eb973e7955f2bd86dc4f0
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2026-03-30 11:54:22 +0200

    fix(test): set RANK/WORLD_SIZE from SLURM env vars for multi-GPU test

--- slurm/test_multigpu.sh
+++ slurm/test_multigpu.sh
@@ -15,9 +15,21 @@ source .venv/bin/activate
 
 srun python -c "
 import torch, torch.distributed as dist, os
+
+# Map SLURM env vars to PyTorch distributed (same as fairseq2's SlurmHandler)
+os.environ['RANK'] = os.environ['SLURM_PROCID']
+os.environ['WORLD_SIZE'] = os.environ['SLURM_NTASKS']
+os.environ['LOCAL_RANK'] = os.environ['SLURM_LOCALID']
+os.environ['LOCAL_WORLD_SIZE'] = os.environ['SLURM_NTASKS_PER_NODE']
+os.environ.setdefault('MASTER_ADDR', 'localhost')
+os.environ.setdefault('MASTER_PORT', '29500')
+
+local_rank = int(os.environ['LOCAL_RANK'])
+torch.cuda.set_device(local_rank)
+
 dist.init_process_group('nccl')
 r = dist.get_rank()
-t = torch.ones(1, device=f'cuda:{os.environ.get(\"LOCAL_RANK\", 0)}') * r
+t = torch.ones(1, device=f'cuda:{local_rank}') * r
 dist.all_reduce(t)
 print(f'Rank {r}: all_reduce = {t.item()}')
 dist.destroy_process_group()
